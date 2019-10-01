8b33dc07a2fb7b990c50ed101c1922b78ab8779e
John Paul Dala <jaypeedala31@gmail.com>
2019-10-01 18:18:51 +0800

    Add dashboard in admin

--- resources/views/backend/dashboard.blade.php
+++ resources/views/backend/dashboard.blade.php
@@ -7,12 +7,54 @@
         <div class="col">
             <div class="card">
                 <div class="card-header">
-                    <strong>@lang('strings.backend.dashboard.welcome') {{ $logged_in_user->name }}!</strong>
+                    <strong>Total Users</strong>
                 </div><!--card-header-->
                 <div class="card-body">
-                    {!! __('strings.backend.welcome') !!}
+                    <h3>{!! $users->total . ' Users' !!}</h3>
+                </div><!--card-body-->
+            </div><!--card-->
+        </div><!--col-->
+        <div class="col">
+            <div class="card">
+                <div class="card-header">
+                        <strong>Total Products</strong>
+                </div><!--card-header-->
+                <div class="card-body">
+                        <h3>{!! $products->total . ' Products' !!}</h3>
+                </div><!--card-body-->
+            </div><!--card-->
+        </div><!--col-->
+        <div class="col">
+            <div class="card">
+                <div class="card-header">
+                        <strong>Total Services</strong>
+                </div><!--card-header-->
+                <div class="card-body">
+                        <h3>{!! $services->total . ' Services' !!}</h3>
+                </div><!--card-body-->
+            </div><!--card-->
+        </div><!--col-->
+        <div class="col">
+            <div class="card">
+                <div class="card-header">
+                        <strong>Total Branches</strong>
+                </div><!--card-header-->
+                <div class="card-body">
+                        <h3>{!! $branches->total . ' Branches' !!}</h3>
                 </div><!--card-body-->
             </div><!--card-->
         </div><!--col-->
     </div><!--row-->
+
+    <!-- Charts -->
+    @include('backend.dashboard.charts')
 @endsection
+
+@push('after-scripts')
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.2/Chart.js" charset="utf-8"></script>
+{!! $userChart->script() !!}
+{!! $productChart->script() !!}
+{!! $orderChart->script() !!}
+{!! $totalOrderChart->script() !!}
+
+@endpush
--- resources/views/frontend/mail/order-details.blade.php
+++ resources/views/frontend/mail/order-details.blade.php
@@ -3,7 +3,7 @@
 @section("content")
 
 	<h1 style="font-size: 18px; font-weight: 300;">Hi {{ $user->first_name . ' ' . $user->last_name }}!</h1>
-	<h3 style="font-size: 18px; font-weight: 300;">Welcome to PACSA National</h3>
+	<h3 style="font-size: 18px; font-weight: 300;">Welcome to Ortiz Skin Clinic </h3>
 
 	<p style="font-size: 14px; font-weight: 300;">Thank you for ordering our products in Ortiz Skin Clinic.</p>
 	<p style="font-size: 14px; font-weight: 300;">Here's your Order Details</p>
@@ -15,7 +15,7 @@
     <p style="font-size: 14px; font-weight: 300;">Total Amount : {{ $order->format_amount }}</p>
 
 	
-	<a href="{{ route('frontend.auth.login') }}" style="font-size: 12px; background: #1e87f0; border: 1px solid transparent; margin: 0; border: none; overflow: visible;color: white; text-transform: none; display: inline-block; box-sizing: border-box; padding: 0 30px; vertical-align: middle; line-height: 38px; text-align: center; text-decoration: none; text-transform: uppercase; -webkit-transition: .1s ease-in-out; transition: .1s ease-in-out; -webkit-transition-property: color,background-color,border-color; transition-property: color,background-color,border-color; margin-top: 20px; border-radius: 3px;">Go To PACSA</a>
+	<a href="{{ route('frontend.auth.login') }}" style="font-size: 12px; background: #1e87f0; border: 1px solid transparent; margin: 0; border: none; overflow: visible;color: white; text-transform: none; display: inline-block; box-sizing: border-box; padding: 0 30px; vertical-align: middle; line-height: 38px; text-align: center; text-decoration: none; text-transform: uppercase; -webkit-transition: .1s ease-in-out; transition: .1s ease-in-out; -webkit-transition-property: color,background-color,border-color; transition-property: color,background-color,border-color; margin-top: 20px; border-radius: 3px;">Go To Ortiz Skin Clinic</a>
 	
 	<p style="font-size: 14px; font-weight: 300; margin-top: 20px;">Should you have any questions, please do not hesitate to contact us.</p>
 
