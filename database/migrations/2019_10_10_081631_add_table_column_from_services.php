bffe5a8e43be817b4469e227282ba7ad3d4d2fd7
John Paul Dala <jaypeedala31@gmail.com>
2019-10-28 18:30:33 +0800

    migration file class name changed

--- database/migrations/2019_10_10_081631_add_table_column_from_services.php
+++ database/migrations/2019_10_10_081631_add_table_column_from_services.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddTableColumnFromServices2 extends Migration
+class AddTableColumnFromServicesTwo extends Migration
 {
     /**
      * Run the migrations.
