d444071e4c3a562fa7bebf5d1ea6deb95c613155
root <root@ip-172-31-29-196.us-east-2.compute.internal>
2019-10-28 10:31:06 +0000

    migration file class name changed

--- database/migrations/2019_10_08_181925_add_table_column_from_reservation.php
+++ database/migrations/2019_10_08_181925_add_table_column_from_reservation.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddTableColumnFromReservations1 extends Migration
+class AddTableColumnFromReservation extends Migration
 {
     /**
      * Run the migrations.
