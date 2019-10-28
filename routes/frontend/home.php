1cc4fdca5e365b1619b0c0705b943ceed92797d5
John Paul Dala <jaypeedala31@gmail.com>
2019-10-28 11:08:18 +0000

    fix branch route

--- routes/frontend/home.php
+++ routes/frontend/home.php
@@ -13,7 +13,7 @@ use App\Http\Controllers\Frontend\User\DashboardController;
  */
 Route::get('/', [HomeController::class, 'index'])->name('index');
 Route::get('contact', [ContactController::class, 'index'])->name('contact');
-Route::get('branches', [BranchListController::class, 'index'])->name('branch');
+#Route::get('branches', [BranchListController::class, 'index'])->name('branch');
 Route::post('contact/send', [ContactController::class, 'send'])->name('contact.send');
 
 /*
