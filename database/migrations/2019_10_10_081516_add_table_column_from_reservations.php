96d0942faa8da4ea2a8360451995662609db94a1
John Paul Dala <jaypeedala31@gmail.com>
2019-10-28 17:59:56 +0800

    migration class change name

--- database/migrations/2019_10_10_081516_add_table_column_from_reservations.php
+++ database/migrations/2019_10_10_081516_add_table_column_from_reservations.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddTableColumnFromReservations extends Migration
+class AddTableColumnFromReservations2 extends Migration
 {
     /**
      * Run the migrations.
--- database/migrations/2019_10_17_125642_add_table_column_from_reservations.php
+++ database/migrations/2019_10_17_125642_add_table_column_from_reservations.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddTableColumnFromReservations extends Migration
+class AddTableColumnFromReservations3 extends Migration
 {
     /**
      * Run the migrations.
